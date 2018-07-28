af39b4dbafebefd0d75e0da46e338f0f60a2e43b
Maik <hellekin87@googlemail.com>
2018-07-28 21:07:46 +0200

    part 4

--- VueJS youtube tutorial/app.js
+++ VueJS youtube tutorial/app.js
@@ -2,7 +2,9 @@ new Vue({
     el: "#vue-app",
     data:{
         name: "maik",
-        job: "ITler"
+        job: "ITler",
+        website: "http://www.google.de",
+        websiteTag: '<a href="http://www.google.de"> Google Site from Tag</a>'
     }, 
     methods: {
         greet:function(time){
