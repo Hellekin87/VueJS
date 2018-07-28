a34694baa40fda17fe9b2d03054f7a6020b068bb
Maik <hellekin87@googlemail.com>
2018-07-28 21:57:49 +0200

    part 5

--- VueJS youtube tutorial/app.js
+++ VueJS youtube tutorial/app.js
@@ -3,13 +3,26 @@ new Vue({
     data:{
         name: "maik",
         job: "ITler",
+        age: 25,
         website: "http://www.google.de",
-        websiteTag: '<a href="http://www.google.de"> Google Site from Tag</a>'
+        websiteTag: '<a href="http://www.google.de"> Google Site from Tag</a>',
+        x: 0,
+        y: 0
     }, 
     methods: {
         greet:function(time){
             var name = this.name;
             return "Good "+time+" "+name;
+        },
+        add:function(){
+            this.age += 1; 
+        },
+        sub:function(){
+            this.age -= 1; 
+        },
+        updateXY:function(event){
+            this.x = event.offsetX;
+            this.y = event.offsetY;
         }
     }
 });
